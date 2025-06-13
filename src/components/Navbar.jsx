16e89858ac884260fa0190f6295d8e92482f48c0
vansh <vanshsin784@gmail.com>
2025-06-14 05:01:26 +0530

    updating the navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,69 +1,78 @@
 import axios from "axios";
 import { useDispatch, useSelector } from "react-redux";
-import { Link, useNavigate } from "react-router";
+import { Link, useNavigate } from "react-router-dom";
 import { deleteUser } from "../utils/userSlice";
+import { useState } from "react";
+
 const Navbar = () => {
-  const dispatch=useDispatch();
-  const navigate=useNavigate();
-  const handleLogout=async()=>{
-    try{
-      await axios.post("https://devtinder-b42n.onrender.com/logout",{
-        withCredentials:true,
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const user = useSelector((store) => store.user);
+  const [selectedOption, setSelectedOption] = useState("");
+
+  const handleLogout = async () => {
+    try {
+      await axios.post("https://devtinder-b42n.onrender.com/logout", {}, {
+        withCredentials: true,
       });
       dispatch(deleteUser());
       navigate("/login");
-
-    } catch(err){
+    } catch (err) {
       console.error(err);
     }
-  }
-  const user=useSelector(store=>store.user);
+  };
+
+  const handleOptionChange = (e) => {
+    const value = e.target.value;
+    setSelectedOption(value);
+
+    if (value === "connections") navigate("/connections");
+    else if (value === "requests") navigate("/recieved/requests");
+    else if (value === "chat") navigate("/chat/123"); // replace 123 with actual id if dynamic
+  };
+
   return (
-      <div className="navbar bg-base-300">
-        <div className="flex-1">
-          <Link to="/" className="btn btn-ghost text-xl"> {"</>"} devTinder</Link>
-        </div>
-        {/* <p className="text-center">Welcome, {user.firstName}</p> */}
-        <div className="flex-none gap-2">
-          {user&&<div className="dropdown dropdown-end mx-5">
-            <div
-              tabIndex={0}
-              role="button"
-              className="btn btn-ghost btn-circle avatar"
-            >
-              <div className="w-10 rounded-full">
-                <img
-                  alt="Tailwind CSS Navbar component"
-                  src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"
-                />
-              </div>
-            </div>
-            <ul
-              tabIndex={0}
-              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-4 shadow"
-            >
-              <li className="mt-1">
-                <Link to="/profile" className="justify-between">
-                  {user.firstName}
-                  <span className="badge">New</span>
-                </Link>
-              </li>
-              <li className="mt-1">
-                <Link to="/connections">Connections</Link>
-              </li>
-              <li className="mt-1">
-                <Link to="/recieved/requests">All Requests</Link>
-              </li>
-              <li className="mt-1">
-                <Link to="/chat/:connectionId">Chat</Link>
-              </li>
-              <li className="mt-1">
-                <a onClick={handleLogout}>Logout</a>
-              </li>
-            </ul>
-          </div>}
-        </div>
+    <div className="navbar bg-base-300">
+      <div className="flex-1">
+        <Link to="/" className="btn btn-ghost text-xl">{"</>"} devTinder</Link>
       </div>
+
+      <div className="flex-none gap-4 items-center">
+        {/* Dropdown for other routes */}
+        {user && (
+          <select
+            className="select select-bordered"
+            value={selectedOption}
+            onChange={handleOptionChange}
+          >
+            <option value="">Explore</option>
+            <option value="connections">Connections</option>
+            <option value="requests">Requests</option>
+            <option value="chat">Chat</option>
+          </select>
+        )}
+
+        {/* Edit Profile Button */}
+        {user && (
+          <button
+            onClick={() => navigate("/profile")}
+            className="btn btn-outline btn-sm"
+          >
+            Edit Profile
+          </button>
+        )}
+
+        {/* Logout Button */}
+        {user && (
+          <button
+            onClick={handleLogout}
+            className="btn btn-error btn-sm text-white"
+          >
+            Logout
+          </button>
+        )}
+      </div>
+    </div>
   );
 };
 
