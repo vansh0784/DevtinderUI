cd448cd88612ee96fa1d181f94e61c0be4a523c7
vansh <vanshsin784@gmail.com>
2025-06-14 07:50:51 +0530

    updating the routes

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { useParams, useNavigate, Link } from "react-router";
+import { useParams, useNavigate, Link } from "react-router-dom";
 import { createSocketConnection } from "../utils/socket";
 import { useSelector, useDispatch } from "react-redux";
 import { allConnect } from "../utils/allConnections";
